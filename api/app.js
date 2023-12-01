c13c4a5c22270abbeda1fda9b31dbe597112fd97
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-02 00:09:41 +0100

    accessing the form data

--- api/app.js
+++ api/app.js
@@ -4,13 +4,14 @@ const axios = require("axios")
 const app = express();
 app.set("view engine", "ejs")
 
+app.use(express.urlencoded())
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/api_form.html")
 })
 
 app.post("/", async (req, res) => {
-
+    console.log(req.body.cityName)
     let response = await axios.post(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${"cbd154e0f1c5ef15a3b8eae701d21971"}`)
     console.log("STATUS CODE ", response.status)
     res.json(response.data)
