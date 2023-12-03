835d45bb24c9e29355a69f2297fedcf7be33300b
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-03 02:19:15 +0100

    random

--- RestAPI/app.js
+++ RestAPI/app.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded());
 
+app.get("/", (req, res) => {
+    res.write("Howdy Mate")
+    res.send()
+})
 
 app.listen(3000, (req, res) => {
     console.log("Listening on port 3000")
