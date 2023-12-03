579457b52e8224a477f3d1b9c602ac00d7681a69
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-12-03 18:51:57 +0100

    READ AND CREATE METHODS ADDED

--- RestAPI/app.js
+++ RestAPI/app.js
@@ -2,34 +2,62 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.urlencoded());
+const { v4: uuidv4 } = require('uuid')
 
+app.use(express.json());
 
 const users = [
     {
-        "firstName": "Peter",
-        "lastName": "Parker",
-        "country": "Nigeria"
+        firstName: 'Kentavious',
+        lastName: 'Cadwell',
+        country: 'Pope',
+        id: 'fa9079ef-4dd2-45a6-b884-e87eaddadd4d'
     },
     {
-        "firstName": "King",
-        "lastName": "Cobra",
-        "country": "Nicaragua"
+        firstName: 'Jordan',
+        lastName: 'Terell',
+        country: 'Carter',
+        id: 'ea2b8d38-40bc-4f49-8aab-ed65bed42d49'
     },
     {
-        "firstName": "Philip",
-        "lastName": "I",
-        "country": "Macedonia"
+        firstName: 'Nyadavious',
+        lastName: 'Wilburn',
+        country: 'Atlanta, GA, USA',
+        id: '66df4d6c-d530-4ce0-890a-66ff682554e8'
     }
-
-
 ]
-
-
-app.get("/", (req, res) => {
+//Get all Users
+app.get("/users", (req, res) => {
     res.send(users)
     console.log(users)
 })
+
+//Get a single User
+app.get("/users/:id", (req, res) => {
+    const query_id = req.params.id
+    console.log(query_id)
+    const data = users.find((user) => user.id === query_id)
+    console.log("data", data)
+    res.send(data)
+})
+
+
+//Create new User 
+app.post("/users", (req, res) => {
+    const data = req.body;
+    const u_id = uuidv4();
+    const updated_data = { ...data, id: u_id }
+
+    console.log("body", updated_data)
+
+    users.push(updated_data)
+
+    res.send(200, updated_data)
+    console.log(users)
+
+})
+
+
 
 app.listen(3000, (req, res) => {
     console.log("Listening on port 3000")
